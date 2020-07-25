434cf8ba84a707c64718ca7050741f284d68d3d4
Andrew Lewis <nerf@judo.za.org>
2020-07-25 02:33:05 +0200

    Add missing elements of protocol response

--- rspamd.go
+++ rspamd.go
@@ -1,15 +1,31 @@
 package main
 
+type Milter struct {
+	RemoveHeaders map[string]int8            `json:"remove_headers"`
+	AddHeaders    map[string]MilterAddHeader `json:"add_headers"`
+}
+
+type MilterAddHeader struct {
+	Order string `json:"order"`
+	Value string `json:"value"`
+}
+
+type Symbol struct {
+	Name    string   `json:"name"`
+	Options []string `json:"options"`
+	Score   float32  `json:"score"`
+}
+
 type RspamdResult struct {
-	Score         float32           `json:"score"`
-	RequiredScore float32           `json:"required_score"`
 	Action        string            `json:"action"`
+	DKIMSignature string            `json:"dkim-signature"`
+	Emails        []string          `json:"emails"`
+	MessageId     string            `json:"message-id"`
 	Messages      map[string]string `json:"messages"`
-	Milter        struct {
-		RemoveHeaders map[string]int8        `json:"remove_headers"`
-		AddHeaders    map[string]interface{} `json:"add_headers"`
-	} `json:"milter"`
-	Symbols map[string]struct {
-		Score float32
-	} `json:"symbols"`
+	Milter        Milter            `json:"milter"`
+	RequiredScore float32           `json:"required_score"`
+	Score         float32           `json:"score"`
+	Skipped       bool              `json:"is_skipped"`
+	Symbols       map[string]Symbol `json:"symbols"`
+	URLs          []string          `json:"urls"`
 }
